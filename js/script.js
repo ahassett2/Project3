7cf587daf97c85f546c1c0f5243b9fc3d17e2a00
ahassett2 <143529575+ahassett2@users.noreply.github.com>
2023-11-15 10:21:15 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,39 +1,31 @@
 var map;
 function initMap() {
-
   map = new google.maps.Map(document.getElementById('map'), {
-    //center: {lat: 41.8741, lng: -87.6285}, 
-    center: {lat: 41.8784, lng: -87.6283},
-    zoom: 15
+    center: {lat: 41.693468631466686, lng: -87.67967437837038  },
+    zoom: 15,
   });
 
-  var school = new google.maps.Marker({
-    position: { lat: 41.8730, lng: -87.6279},
-    map: map
+  var marker = new google.maps.Marker({
+    position: { lat: 41.693468631466686, lng: -87.67967437837038  },
+    map: map,
   });
 
-  var reckless = new google.maps.Marker({
-    position: { lat: 41.882324790101684, lng: -87.62668825420475 },
-    map: map
+  var infowindow = new google.maps.InfoWindow({
+    content: "This is where I went to school",
   });
+  infowindow.open(map, marker);
 
-  var distPoints = [
-    { lat: 41.8730, lng: -87.6279 },
-    { lat: 41.882051955922336, lng: -87.62782201616926 },
-    { lat: 41.88206477020562, lng: -87.62670524486201 },
-    { lat: 41.882324790101684, lng: -87.62668825420475 },
+  var features = [
+    { position: { lat: 41.693468631466686, lng: -87.67967437837038  }, type: "Home" },
   ];
 
-  var dist = new google.maps.Polyline({
-    path: distPoints,
-    geodesic: true,
-    strokeColor: "#FE5F55",
-    strokeOpacity: 1.0,
-    strokeWeight: 2,
-  });
-
-  dist.setMap(map);
-
+  for (let i = 0; i < features.length; i++) {
+    const marker = new google.maps.Marker({
+      position: features[i].position,
+      icon: icons[features[i].type].icon,
+      map: map,
+    });
+  }
 }
 
-window.initMap = initMap;
+google.maps.event.addDomListener(window, 'load', initMap);
